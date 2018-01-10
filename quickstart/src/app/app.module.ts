02821bb67a9d61307f535d9e6b89e515a7028867
eruljohnubas <erulggubas@yahoo.com>
2018-01-10 10:10:29 +0800

    modified files on title

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title.component';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { TitleComponent } from './title.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+ 
+ providers: [ ContactService, UserService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
