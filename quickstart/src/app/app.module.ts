1bd30bd9700646b257bee5f0098654f684b9e4ae
eruljohnubas <erulggubas@yahoo.com>
2018-01-10 10:01:23 +0800

    modified components

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+  AppComponent,
+  HighlightDirective,
+  TitleComponent,
+],
   imports: [
     BrowserModule
   ],
