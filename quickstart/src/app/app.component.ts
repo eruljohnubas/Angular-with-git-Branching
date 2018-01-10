94b5eef23eea2306def4a31118df7e7e36d3e349
alzekiel09 <aixonairashii@gmail.com>
2018-01-10 11:03:20 +0800

    router linked

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: template: `
   template: `
-  <app-title></app-title>
-  <nav>
-    <a routerLink="contact" routerLinkActive="active">Contact</a>
-    <a routerLink="crisis"  routerLinkActive="active">Crisis Center</a>
-    <a routerLink="heroes"  routerLinkActive="active">Heroes</a>
-  </nav>
-  <router-outlet></router-outlet>
-`
+    <app-title></app-title>
+    <nav>
+      <a routerLink="contact" routerLinkActive="active">Contact</a>
+      <a routerLink="crisis"  routerLinkActive="active">Crisis Center</a>
+      <a routerLink="heroes"  routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {}
